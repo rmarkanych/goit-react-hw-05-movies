89147c34a670012fad58cc69d41d5f9deffc1996
Roman <roman.markanych@gmail.com>
2022-09-26 13:48:18 +0300

    add reacat-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
+import { Routes, Route, NavLink } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Movies from '../pages/Movies/Movies';
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink to="home/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<p>Page not found</p>} />
+      </Routes>
     </div>
   );
 };
+export default App;
